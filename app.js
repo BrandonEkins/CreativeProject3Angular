84cc78abb8a95627b0e2c14ac52b888133975b71
Brandon Ekins <brandon.ekins@gmail.com>
2016-10-11 15:42:53 -0600

    added some basics

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-angular.module('app', [])
+angular.module('app', ['ngMaterial'])
     .controller('mainCtrl', [
         '$scope',
         function($scope) {
             $scope.test = "Hello World";
+            $scope.currentMonth = {};
+            $scope.calander = [{
+                name: "January",
+                days: [],
+            }, {
+                name: "Febuary",
+                days: [],
+            }, {
+                name: "March",
+                days: [],
+            }, {
+                name: "April",
+                days: [],
+            }, {
+                name: "May",
+                days: [],
+            }, {
+                name: "June",
+                days: [],
+            }, {
+                name: "July",
+                days: [],
+            }, {
+                name: "August",
+                days: [],
+            }, {
+                name: "September",
+                days: [],
+            }, {
+                name: "October",
+                days: [],
+            }, {
+                name: "November",
+                days: [],
+            }, ]
+            $scope.setDate = function() {
+                var my_date = new Date();
+                $scope.currentMonth = $scope.calander[my_date.getMonth()];
+            }
+            $scope.setDate();
+
+
+
+
+
+
+
+
+
         }
     ]);
