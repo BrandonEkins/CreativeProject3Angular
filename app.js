f2ec80b006bfebd4ec78e4469fba8c12c408af00
Brandon Ekins <brandon.ekins@gmail.com>
2016-10-12 17:14:00 -0600

    fdsa

--- app.js
+++ app.js
@@ -5,45 +5,62 @@ angular.module('app', ['ngMaterial'])
             $scope.test = "Hello World";
             $scope.currentMonth = {};
             $scope.calander = [{
-                name: "January",
-                days: [],
-            }, {
-                name: "Febuary",
-                days: [],
-            }, {
-                name: "March",
-                days: [],
-            }, {
-                name: "April",
-                days: [],
-            }, {
-                name: "May",
-                days: [],
-            }, {
-                name: "June",
-                days: [],
-            }, {
-                name: "July",
-                days: [],
-            }, {
-                name: "August",
-                days: [],
-            }, {
-                name: "September",
-                days: [],
-            }, {
-                name: "October",
-                days: [],
-            }, {
-                name: "November",
-                days: [],
-            }, ]
+                    name: "January",
+                    days: [],
+                }, {
+                    name: "Febuary",
+                    days: [],
+                }, {
+                    name: "March",
+                    days: [],
+                }, {
+                    name: "April",
+                    days: [],
+                }, {
+                    name: "May",
+                    days: [],
+                }, {
+                    name: "June",
+                    days: [],
+                }, {
+                    name: "July",
+                    days: [],
+                }, {
+                    name: "August",
+                    days: [],
+                }, {
+                    name: "September",
+                    days: [],
+                }, {
+                    name: "October",
+                    days: [],
+                }, {
+                    name: "November",
+                    days: [],
+                },
+                {
+                    name: "December",
+                    days: [],
+                },
+            ]
+            $scope.daysInEachMonth = [31, 28, 30, 31, ]
+            $scope.calcDays = function() {
+
+            }
             $scope.setDate = function() {
                 var my_date = new Date();
                 $scope.currentMonth = $scope.calander[my_date.getMonth()];
+                $scope.curMonthInt = my_date.getMonth();
             }
             $scope.setDate();
-
+            $scope.forwardMonth = function() {
+                $scope.curMonthInt += 1;
+                $scope.currentMonth = $scope.calander[$scope.curMonthInt]
+            }
+            $scope.backMonth = function() {
+                $scope.curMonthInt -= 1;
+                $scope.currentMonth = $scope.calander[$scope.curMonthInt]
+            }
 
 
 
