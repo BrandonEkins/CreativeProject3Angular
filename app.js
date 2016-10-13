2a3f16d53c8c2ee3f94bc82ea5ae25b1cff824d8
Brandon Ekins <brandon.ekins@gmail.com>
2016-10-12 19:29:28 -0600

    fdsa

--- app.js
+++ app.js
@@ -43,10 +43,33 @@ angular.module('app', ['ngMaterial'])
                     days: [],
                 },
             ]
-            $scope.daysInEachMonth = [31, 28, 30, 31, ]
-            $scope.calcDays = function() {
-
+            $scope.day = {
+                date: 0,
+                dayOfWeek: "",
+                events: [] // work on this
             }
+            $scope.daysInEachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            $scope.daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            $scope.currentDayOfWeek = 4;
+            $scope.calcDays = function() {
+                for (var i = 0; $scope.calander.length > i; i++) { //selects correct month
+
+                    for (var j = 0; $scope.daysInEachMonth[i] > j; j++) {
+                        $scope.calander[i].days.push({ // adds days to day array
+                            date: j,
+                            dayOfWeek: $scope.daysOfWeek[$scope.currentDayOfWeek],
+                            events: []
+                        });
+                        if ($scope.currentDayOfWeek == 6) { // increments days of week
+                            $scope.currentDayOfWeek = 0;
+                        } else {
+                            $scope.currentDayOfWeek += 1;
+                        }
+                    }
+
+                }
+            }
+            $scope.calcDays();
             $scope.setDate = function() {
                 var my_date = new Date();
                 $scope.currentMonth = $scope.calander[my_date.getMonth()];
